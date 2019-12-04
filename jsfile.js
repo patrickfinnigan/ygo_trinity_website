9d1507b8a0f33df256bef4860793c8c1fec1566a
patrickfinnigan <patrickfinnigan94@gmail.com>
2019-12-04 06:54:18 -0800

    was hardcoding the banlist but remembered how to just use jquery lol

--- jsfile.js
+++ jsfile.js
@@ -1,3 +1,17 @@
+var unbannedList = [
+    {cardName: "Danger! Bigfoot! (Was Co-Forbidden)", cardType: "banlist-effect"},
+    {cardName: "Witchcrafter Golem Aruru (Was Co-Forbidden)", cardType: "banlist-effect"},
+    {cardName: "Salamangreat Falco (Was Forbidden)", cardType: "banlist-effect"},
+    {cardName: "Fortune Lady Every (Was Co-Forbidden)", cardType: "banlist-synchro"},
+    {cardName: "Toadally Awesome (Was Co-Forbidden)", cardType: "banlist-xyz"},
+    {cardName: "Evil Eye Domain – Pareidolia (Was Forbidden)", cardType: "banlist-spell"}
+]
+
+for (let i = 0; i < unbannedList.length; i++) {
+    console.log(unbannedList[i]);
+    $(unbannedCards).append("<li class='list-group-item banlist " + unbannedList[i].cardType + "'>" + unbannedList[i].cardName + "</li>")
+}
+
 $(window).scroll(function () {
     var distanceFromTop = $(this).scrollTop();
     if (distanceFromTop >= $('.img-fluid').height()) {
